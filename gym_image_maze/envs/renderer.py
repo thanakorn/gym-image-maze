26ccc742ff31e404a03d43a9fefc9e4df5d581f4
thanakorn <thanakorn.panyapiang@outlook.com>
2020-08-25 16:11:51 +0100

    Enable run without screen

--- gym_image_maze/envs/renderer.py
+++ gym_image_maze/envs/renderer.py
@@ -2,13 +2,10 @@ import numpy as np
 import pyglet
 import ctypes
 
-from pyglet.window import Window
-from pyglet.image import ImageData
-from pyglet.gl import *
-
 class Renderer(object):
     @classmethod
     def get_window(cls, display, width, height):
+        from pyglet.window import Window
         screen = display.get_screens()[0]
         config = screen.get_best_config()
         context = config.create_context(None)
@@ -21,6 +18,8 @@ class Renderer(object):
         self.window = Renderer.get_window(self.display, width, height) 
     
     def render(self, img_array):
+        from pyglet.image import ImageData
+        from pyglet.gl import glClearColor
         glClearColor(1,1,1,1)
         img_array = np.ascontiguousarray(img_array[::-1, :])
         img_c_array = img_array.ctypes.data_as(ctypes.POINTER(ctypes.c_float * img_array.size))
